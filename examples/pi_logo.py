3c57a320a6dae3585c41446982732030e7f12e39
Sascha <Gizu@users.noreply.github.com>
2016-04-30 23:41:07 +0200

    Update pi_logo.py

--- examples/pi_logo.py
+++ examples/pi_logo.py
@@ -4,7 +4,7 @@ from oled.device import ssd1306, sh1106
 from oled.render import canvas
 from PIL import ImageDraw, Image
 
-device = sh1106(port=2, address=0x3C)
+device = sh1106(port=1, address=0x3C)
 
 with canvas(device) as draw:
     logo = Image.open('examples/images/pi_logo.png')
