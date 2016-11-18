d982724efa4415f4ed596590bf0d1abd1aa8c056
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-18 23:39:42 +0000

    Fix regression after f0584c1

--- oled/device.py
+++ oled/device.py
@@ -164,7 +164,7 @@ class sh1106(device):
         page = 0xB0
         pix = list(image.getdata())
         step = self.width * 8
-        for y in range(0, self.pages * step, step):
+        for y in range(0, self._pages * step, step):
 
             # move to given page, then reset the column address
             self.command(page, 0x02, 0x10)
