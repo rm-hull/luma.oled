43c91c161e8e5f65f516baddc81210bbe8f91c30
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-18 23:38:47 +0000

    Reduce duplication

--- examples/demo.py
+++ examples/demo.py
@@ -9,10 +9,15 @@ from demo_opts import device
 from oled.render import canvas
 from PIL import ImageFont
 
+# Load default font.
 font = ImageFont.load_default()
 
-print("Testing basic canvas graphics...")
-with canvas(device) as draw:
+# Alternatively load a TTF font.
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+
+def primitives(draw):
     # Draw some shapes.
     # First define some constants to allow easy resizing of shapes.
     padding = 2
@@ -20,7 +25,7 @@ with canvas(device) as draw:
     top = padding
     bottom = device.height - padding - 1
     # Draw a rectangle of the same size of screen
-    draw.rectangle((0, 0, device.width - 1, device.height - 1), outline=255, fill=0)
+    draw.rectangle(device.bounding_box, outline=255, fill=0)
     # Move left to right keeping track of the current x position for drawing shapes.
     x = padding
     # Draw an ellipse.
@@ -36,37 +41,39 @@ with canvas(device) as draw:
     draw.line((x, bottom, x + shape_width, top), fill=255)
     draw.line((x, top, x + shape_width, bottom), fill=255)
     x += shape_width + padding
-
-    # Load default font.
-    font = ImageFont.load_default()
-
-    # Alternatively load a TTF font.
-    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-    # font = ImageFont.truetype('Minecraftia.ttf', 8)
-
     # Write two lines of text.
     draw.text((x, top),    'Hello',  font=font, fill=255)
     draw.text((x, top + 20), 'World!', font=font, fill=255)
 
-time.sleep(10)
 
-print("Testing display ON/OFF...")
-for _ in range(10):
-    time.sleep(0.5)
-    device.hide()
-
-    time.sleep(0.5)
-    device.show()
-
-print("Testing clear display...")
-time.sleep(2)
-device.clear()
-
-print("Testing screen updates...")
-time.sleep(2)
-for x in range(30):
+def main():
+    print("Testing basic canvas graphics...")
     with canvas(device) as draw:
-        now = datetime.datetime.now()
-        draw.text((x, 10), str(now.date()), fill=255)
-        draw.text((10, 24), str(now.time()), fill=255)
-        time.sleep(1)
+        primitives(draw)
+
+    time.sleep(10)
+
+    print("Testing display ON/OFF...")
+    for _ in range(10):
+        time.sleep(0.5)
+        device.hide()
+
+        time.sleep(0.5)
+        device.show()
+
+    print("Testing clear display...")
+    time.sleep(2)
+    device.clear()
+
+    print("Testing screen updates...")
+    time.sleep(2)
+    for x in range(30):
+        with canvas(device) as draw:
+            now = datetime.datetime.now()
+            draw.text((x, 10), str(now.date()), fill=255)
+            draw.text((10, 24), str(now.time()), fill=255)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
