03ff0aae43d8f69f7211cced46cf3e6b482ffd71
Thijs Triemstra <info@collab.nl>
2016-11-30 03:13:44 +0100

    extend keywords

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
                  "SSD1306 or SH1106 chipset"),
     long_description=README,
     license="MIT",
-    keywords="raspberry pi rpi oled ssd1306 sh1106",
+    keywords="raspberry pi rpi oled display screen ssd1306 sh1106 spi i2c",
     url="https://github.com/rm-hull/ssd1306",
     download_url="https://github.com/rm-hull/ssd1306/tarball/" + version,
     packages=["oled"],
