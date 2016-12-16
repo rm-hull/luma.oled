3f04612a37b7169d530f3d1b4d3f26727b2a07ce
Richard Hull <rm_hull@yahoo.co.uk>
2016-12-16 22:15:27 +0000

    Updated/improved PI logo demo
    Now works in colour, and shows some image transformations (rotation /
    composition)

--- examples/pi_logo.py
+++ examples/pi_logo.py
@@ -2,20 +2,30 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 
-import time
 import os.path
 from demo_opts import device
-from oled.render import canvas
 from PIL import Image
 
 
-with canvas(device) as draw:
+def main():
     img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-        'images', 'pi_logo.png'))
-    logo = Image.open(img_path)
-    draw.bitmap((32, 0), logo, fill="white")
+                                            'images', 'pi_logo.png'))
+    logo = Image.open(img_path).convert("RGBA")
+    fff = Image.new(logo.mode, (logo.width, logo.height), (255,) * 4)
 
-try:
-    time.sleep(5)
-except KeyboardInterrupt:
-    pass
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+
+    while True:
+        for angle in range(360):
+            rot = logo.rotate(angle)
+            img = Image.composite(rot, fff, rot)
+            background.paste(img, posn)
+            device.display(background.convert(device.mode))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
