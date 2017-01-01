859f546596e954cb89ff923bb6e28301cdbf4c33
Richard Hull <rm_hull@yahoo.co.uk>
2017-01-01 11:50:57 +0000

    Sense rotation mode, and scroll accordingly

--- examples/carousel.py
+++ examples/carousel.py
@@ -52,8 +52,14 @@ def first(iterable, default=None):
 
 
 def main():
-    widget_width = device.width // 2
-    widget_height = device.height
+    if device.rotate in (0, 2):
+        # Horizontal
+        widget_width = device.width // 2
+        widget_height = device.height
+    else:
+        # Vertical
+        widget_width = device.width
+        widget_height = device.height // 2
 
     # Either function or subclass
     #  cpuload = hotspot(widget_width, widget_height, cpu_load.render)
@@ -75,12 +81,21 @@ def main():
 
     widgets = [cpuload, utime, clk, net_wlan, net_eth, net_lo, mem, dsk]
 
-    virtual = viewport(device, width=widget_width * len(widgets), height=widget_height)
-    for i, widget in enumerate(widgets):
-        virtual.add_hotspot(widget, (i * widget_width, 0))
+    if device.rotate in (0, 2):
+        virtual = viewport(device, width=widget_width * len(widgets), height=widget_height)
+        for i, widget in enumerate(widgets):
+            virtual.add_hotspot(widget, (i * widget_width, 0))
 
-    for x in pause_every(widget_width, position(widget_width * (len(widgets) - 2))):
-        virtual.set_position((x, 0))
+        for x in pause_every(widget_width, position(widget_width * (len(widgets) - 2))):
+            virtual.set_position((x, 0))
+
+    else:
+        virtual = viewport(device, width=widget_width, height=widget_height * len(widgets))
+        for i, widget in enumerate(widgets):
+            virtual.add_hotspot(widget, (0, i * widget_height))
+
+        for y in pause_every(widget_height, position(widget_height * (len(widgets) - 2))):
+            virtual.set_position((0, y))
 
 
 if __name__ == "__main__":
