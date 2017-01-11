b3f2defbb422a021a2650ed8738301a13477a71a
Richard Hull <rm_hull@yahoo.co.uk>
2017-01-11 00:50:44 +0000

    Try again with pkgutil (py27) ?

---
+++ luma/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
