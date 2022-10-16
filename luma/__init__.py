2fb75113eca1075c7332a5dfe447a7596ddd93c3
Thijs Triemstra <info@collab.nl>
2022-10-16 02:05:03 +0200

    Drop support for Python 3.6 (#349)
    * update changelog
    
    * remove py36 classifier
    
    * update changelog
    
    * ci: stop testing python 3.6
    
    * fix Pillow DeprecationWarning
    
    * use f-string
    
    * setup implicit namespace package
    
    * remove deprecated setting

--- luma/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2014-2019 Richard Hull and contributors
-# See LICENSE.rst for details.
-
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
