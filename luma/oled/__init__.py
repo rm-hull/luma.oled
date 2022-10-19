bd4d70d9db671542f33fa58a5b1bb77d67a70294
Thijs Triemstra <info@collab.nl>
2022-10-19 14:59:53 +0200

    bump version: 3.9.0

--- luma/oled/__init__.py
+++ luma/oled/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2014-2020 Richard Hull and contributors
+# Copyright (c) 2014-2022 Richard Hull and contributors
 # See LICENSE.rst for details.
 
 """
@@ -7,4 +7,4 @@ OLED display driver for SSD1306, SSD1309, SSD1322, SSD1325, SSD1327, SSD1331,
 SSD1351, SSD1362 and SH1106 devices.
 """
 
-__version__ = '3.8.1'
+__version__ = '3.9.0'
