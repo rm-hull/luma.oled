250709a439bbde05b89c859ae8cf4b93d58cd707
Thijs Triemstra <info@collab.nl>
2024-10-26 19:15:25 +0200

    fix lint error

--- tests/test_ssd1351.py
+++ tests/test_ssd1351.py
@@ -215,7 +215,7 @@ def test_display():
     assert recordings == get_reference_data('demo_ssd1351')
 
 
-@ pytest.mark.parametrize("bit,expected_16_bit_color", [
+@pytest.mark.parametrize("bit,expected_16_bit_color", [
     (7, [0b10000100, 0b00010000]),
     (6, [0b01000010, 0b00001000]),
     (5, [0b00100001, 0b00000100]),
