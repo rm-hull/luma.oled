6a2e9458e74a3c15eacac2f8d3a739c3fc914a03
Richard Hull <rm_hull@yahoo.co.uk>
2026-05-26 21:03:08 +0100

    Simplify framebuffer_mixin by removing deprecation notice
    Removed deprecation warning for string framebuffer in framebuffer_mixin.

--- luma/oled/device/framebuffer_mixin.py
+++ luma/oled/device/framebuffer_mixin.py
@@ -7,13 +7,7 @@ import luma.core.framebuffer
 
 class __framebuffer_mixin(object):
     """
-    Helper class for initializing the framebuffer. Its only purpose is to
-    log a deprecation warning if a string framebuffer is specified.
-
-    .. note::
-        Specifying the framebuffer as a string will be removed at the next
-        major release, and hence this mixin will become redundant and will
-        also be removed at that point.
+    Helper class for initializing the framebuffer. 
 
     .. versionadded:: 3.8.0
     """
@@ -21,12 +15,5 @@ class __framebuffer_mixin(object):
     def init_framebuffer(self, framebuffer):
         if framebuffer is None:
             self.framebuffer = luma.core.framebuffer.diff_to_previous()
-        elif isinstance(framebuffer, str):
-            import warnings
-            warnings.warn(
-                "Specifying framebuffer as a string is now deprecated; Supply an instance of class full_frame() or diff_to_previous() instead",
-                DeprecationWarning
-            )
-            self.framebuffer = getattr(luma.core.framebuffer, framebuffer)()
         else:
             self.framebuffer = framebuffer
