31056917f1bbc1273662c5ea8d8475c0f917d24a
Richard Hull <rm_hull@yahoo.co.uk>
2026-05-26 21:22:08 +0100

    Add param doc

--- luma/oled/device/framebuffer_mixin.py
+++ luma/oled/device/framebuffer_mixin.py
@@ -9,6 +9,8 @@ class __framebuffer_mixin(object):
     """
     Helper class for initializing the framebuffer.
 
+    :param framebuffer: Typically an instance of class full_frame() or diff_to_previous().
+
     .. versionadded:: 3.8.0
     """
     def init_framebuffer(self, framebuffer):
