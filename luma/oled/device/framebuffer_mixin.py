4e8b3b7e17c1fc380f60ebb06b0744e67e2ee500
Richard Hull <rm_hull@yahoo.co.uk>
2026-05-27 07:46:16 +0100

    Simplify framebuffer_mixin by removing deprecation notice (#423)
    * Simplify framebuffer_mixin by removing deprecation notice
    
    Removed deprecation warning for string framebuffer in framebuffer_mixin.
    
    * Refactor framebuffer initialization in framebuffer_mixin.py
    
    Simplified framebuffer initialization logic by using a single line conditional assignment.
    
    * Fix formatting of docstring in framebuffer_mixin.py
    
    * Add param doc
    
    * Update copyright year
    
    * Update version 3.16.0 description in CHANGES.rst
    
    * Move framebuffer parameter in init_framebuffer
    
    Add parameter documentation for init_framebuffer method.
    
    ---------
    
    Co-authored-by: Thijs Triemstra <info@collab.nl>

--- luma/oled/device/framebuffer_mixin.py
+++ luma/oled/device/framebuffer_mixin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020 Richard Hull and contributors
+# Copyright (c) 2020-2026 Richard Hull and contributors
 # See LICENSE.rst for details.
 
 import luma.core.framebuffer
@@ -7,26 +7,12 @@ import luma.core.framebuffer
 
 class __framebuffer_mixin(object):
     """
-    Helper class for initializing the framebuffer. Its only purpose is to
-    log a deprecation warning if a string framebuffer is specified.
-
-    .. note::
-        Specifying the framebuffer as a string will be removed at the next
-        major release, and hence this mixin will become redundant and will
-        also be removed at that point.
+    Helper class for initializing the framebuffer.
 
     .. versionadded:: 3.8.0
     """
-
     def init_framebuffer(self, framebuffer):
-        if framebuffer is None:
-            self.framebuffer = luma.core.framebuffer.diff_to_previous()
-        elif isinstance(framebuffer, str):
-            import warnings
-            warnings.warn(
-                "Specifying framebuffer as a string is now deprecated; Supply an instance of class full_frame() or diff_to_previous() instead",
-                DeprecationWarning
-            )
-            self.framebuffer = getattr(luma.core.framebuffer, framebuffer)()
-        else:
-            self.framebuffer = framebuffer
+        """
+        :param framebuffer: Typically an instance of class full_frame() or diff_to_previous().
+        """
+        self.framebuffer = framebuffer or luma.core.framebuffer.diff_to_previous()
